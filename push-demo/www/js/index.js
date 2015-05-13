280b69e19ccb4f87cf1b64792d0ca96104a642c9
thefearlesshyena <luke@epimedia.com>
2015-05-13 12:39:21 -0600

    added unique pubnub subscribe and publish keys

--- push-demo/www/js/index.js
+++ push-demo/www/js/index.js
@@ -1,51 +1,91 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
+/* Cordova Push Notification Demo
+ * index.js - this file should be in your_cordova_app_root/www/js
+ * Also, you need PushNotification.js and PubNub.js in the dir too.
+ * Install Cordova Push Plugin w/ CLI...
+ * $ cordova plugin add https://github.com/phonegap-build/PushPlugin.git 
  */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        console.log('Received Event: ' + id);
+ 
+var channel = '';
+var t = document.getElementById('temperature');
+ 
+var pubnub = PUBNUB.init({
+        subscribe_key: 'sub-c-fb0bdcb6-f99e-11e4-b608-02ee2ddab7fe ',
+        publish_key:   'pub-c-244dd819-f95d-4516-a874-8cd2c4b5442f ',
+    });
+ 
+function initialize() {
+    bindEvents();
+}
+function bindEvents() {
+    document.addEventListener('deviceready', init, false);
+}
+ 
+function init() {
+    var pushNotification = window.plugins.pushNotification;
+    pushNotification.register(successHandler, errorHandler, {'senderID':'1044047597679','ecb':'onNotificationGCM'});
+    // Get your Sender ID at cloud.google.com/console
+}
+ 
+function successHandler(result) {
+    console.log('Success: '+ result);
+}
+ 
+function errorHandler(error) {
+    console.log('Error: '+ error);
+}
+ 
+function onNotificationGCM(e) {
+    switch( e.event ){
+        case 'registered':
+            if ( e.regid.length > 0 ){
+                console.log('regid = '+e.regid);
+                deviceRegistered(e.regid);
+            }
+        break;
+ 
+        case 'message':
+            console.log(e);
+            if (e.foreground){
+                alert('The room temperature is set too high')
+            }
+        break;
+ 
+        case 'error':
+            console.log('Error: '+e.msg);
+        break;
+ 
+        default:
+          console.log('An unknown event was received');
+          break;
     }
-};
-
-app.initialize();
+}
+ 
+// Publish the channel name and regid to PubNub
+function deviceRegistered(regid) {
+    channel = regid.substr(regid.length - 8).toLowerCase();
+ 
+    var c = document.querySelector('.channel');
+    c.innerHTML = 'Your Device ID: <strong>' + channel + '</strong>';
+    c.classList.remove('blink'); 
+ 
+    pubnub.publish({
+        channel: channel,
+        message: {
+            regid: regid
+        }
+    });
+ 
+    pubnub.subscribe({
+        channel: channel,
+        callback: function(m) {
+            console.log(m);
+            t.classList.remove('gears');
+            if(m.setting) {
+                t.textContent = m.setting + '°';
+            }
+        }
+    });  
+}
+ 
+ 
+initialize();
